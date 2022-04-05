4036649dffd64de883220f5bb0706e3f1a3ade73
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-04 21:44:16 -0400

    routes added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import pool from "./db/connection.js";
 import passport from "passport";
 import "./auth/jwt-auth.js";
+import router from "./routes/userAuthRoutes.js";
 
 const app = express();
 
@@ -17,5 +18,6 @@ app.use(passport.initialize());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
+  app.use(router);
   console.log(`Server running on port ${PORT}`);
 });
