16f6ba47489083d58244b834b3d921964b227341
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-05 10:35:08 -0400

    password provided through env

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,12 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const { Pool } = pg;
 
 const pool = new Pool({
   user: "christopher",
-  password: "password",
+  password: process.env.PG_PASSWORD,
   host: "localhost",
   database: "user-authentication-restful-api",
   port: 5432,
