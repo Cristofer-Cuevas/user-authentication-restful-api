719ee65c732dfc7dd3b3377b28de0b6fbe08a189
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-05 22:34:52 -0400

    first-commit

---
+++ utils/passwordUtils.js
@@ -0,0 +1,19 @@
+import crypto from "crypto";
+
+export const genPassword = (password) => {
+  const salt = crypto.randomBytes(32).toString("hex");
+  const genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, "sha512").toString("hex");
+  return {
+    hash: genHash,
+    salt: salt,
+  };
+};
+
+export const validatePassword = (password, hash, salt) => {
+  if (!hash || !salt) {
+    return false;
+  } else {
+    const hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, "sha512").toString("hex");
+    return hash === hashVerify;
+  }
+};
