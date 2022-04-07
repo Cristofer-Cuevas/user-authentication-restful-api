42b4dc508c22a0356394e62cc0df4ddb783f8f18
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-06 22:54:41 -0400

    development configuration added

--- db/connection.js
+++ db/connection.js
@@ -4,6 +4,14 @@ dotenv.config();
 
 const { Pool } = pg;
 
+const devConfig = {
+  user: process.env.PG_USER,
+  passowrd: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT,
+};
+
 const pool = new Pool({
   user: "christopher",
   password: process.env.PG_PASSWORD,
